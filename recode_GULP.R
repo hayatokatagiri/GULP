40360fee0944c1538b225cc628598f18f83eb0e8
hayatokatagiri <zurqnd@gmail.com>
2025-05-21 16:58:54 +0900

    read.csvの追加

--- recode_GULP.R
+++ recode_GULP.R
@@ -1,1 +1,12 @@
 library()
+
+#GULPデータの読み込み
+
+df_urban <- read.csv("1468.csv")   #大都市調査
+df_rural <- read.csv("1469.csv")   #非大都市調査
+df_mail <- read.csv("1470.csv")   #全国郵送調査
+
+# 読み込んだデータフレームの先頭を確認（オプション）
+head(df_urban)
+head(df_rural)
+head(df_mail)
