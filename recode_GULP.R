ae79642be8a28d198b34aef82a6125f220378f87
hayatokatagiri <zurqnd@gmail.com>
2025-05-21 22:37:13 +0900

    変数名をsurvey_typeからcity_typeに置換

--- recode_GULP.R
+++ recode_GULP.R
@@ -6,14 +6,14 @@ df_rural <- read.csv("1469.csv")   #非大都市調査
 
 ###マージ（大都市調査と非大都市調査）####
 df_urban_processed <- df_urban %>%
-  mutate(survey_type = "大都市調査") # 新しい変数 'survey_type' を追加し、"大都市調査"という値を入れる
+  mutate(city_type = "大都市調査") # 新しい変数 'city_type' を追加し、"大都市調査"という値を入れる
 df_rural_processed <- df_rural %>%
-  mutate(survey_type = "非大都市調査") # 新しい変数 'survey_type' を追加し、"非大都市調査"という値を入れる
+  mutate(city_type = "非大都市調査") # 新しい変数 'city_type' を追加し、"非大都市調査"という値を入れる
 # 共通の列名だけを選んでデータフレームを整形
 df_urban_selected <- df_urban_processed %>%
-  select(all_of(common_cols), survey_type) # 共通列と新しく追加したsurvey_typeを選択
+  select(all_of(common_cols), city_type) # 共通列と新しく追加したcity_typeを選択
 df_rural_selected <- df_rural_processed %>%
-  select(all_of(common_cols), survey_type) # 共通列と新しく追加したsurvey_typeを選択
+  select(all_of(common_cols), city_type) # 共通列と新しく追加したcity_typeを選択
 # 2つのデータフレームを縦に結合
 # bind_rowsは、列の順序が異なっても、列名が一致すれば正しく結合できる
 # 共通の列だけを選んでいれば問題ない
