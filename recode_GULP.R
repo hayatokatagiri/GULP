7273de0fa129b69b7b63334a8155a7d351b837de
hayatokatagiri <zurqnd@gmail.com>
2025-05-31 16:56:38 +0900

    recode_GULPを修正。sc.rmdを追加。

--- recode_GULP.R
+++ recode_GULP.R
@@ -7,30 +7,17 @@ library(qrecode)
 df_urban <- read.csv("1468.csv")   #大都市調査
 df_rural <- read.csv("1469.csv")   #非大都市調査
 
-###マージ（大都市調査と非大都市調査）####
+#マージ（大都市調査と非大都市調査）
 df_urban_processed <- df_urban %>%
   mutate(city_type = "大都市調査") # 新しい変数 'city_type' を追加し、"大都市調査"という値を入れる
 df_rural_processed <- df_rural %>%
   mutate(city_type = "非大都市調査") # 新しい変数 'city_type' を追加し、"非大都市調査"という値を入れる
-# 共通の列名だけを選んでデータフレームを整形
-df_urban_selected <- df_urban_processed %>%
-  select(all_of(common_cols), city_type) # 共通列と新しく追加したcity_typeを選択
-df_rural_selected <- df_rural_processed %>%
-  select(all_of(common_cols), city_type) # 共通列と新しく追加したcity_typeを選択
-# 2つのデータフレームを縦に結合
-# bind_rowsは、列の順序が異なっても、列名が一致すれば正しく結合できる
-# 共通の列だけを選んでいれば問題ない
-df <- bind_rows(df_urban_selected, df_rural_selected)
----------------------------------------------------------------------
 
-####リコード####
-# 結合されたデータフレームの構造と最初の数行を確認
-str(df)
-head(df)
-tail(df) # データが結合されていることを確認するために最後の方も確認
+df <- bind_rows(df_urban_processed, df_rural_processed)
 
-####属性・SES####
-#年齢
+#リコード###############################
+#属性・SES####
+##年齢####
 table(df$SQ2, useNA='always')
 df$age <- NA
 df$age[df$SQ2 == 0] <- 19
@@ -47,20 +34,20 @@ df$age[df$SQ2 == 10] <- (65 + 69) /2
 df$age[df$SQ2 == 11] <- 70
 table(df$age, useNA='always')
 
-#性別
+##性別####
 table(df$SQ3, useNA='always')
 df$fmd <- NA
 df$fmd[df$SQ3 == 2] <- 1
 df$fmd[df$SQ3 == 1] <- 0
 
-#既婚ダミー
+##既婚ダミー####
 table(df$F1, useNA='always')
 df$married <- NA
 df$married[df$F1 == 1] <- 1
 df$married[df$F1 == 2 |df$F1 == 3] <- 0
 table(df$married)
 
-#教育年数
+##教育年数####
 table(df$F5, useNA='always')
 df$edu <- NA
 df$edu[df$F5 == 1] <- 7.5
@@ -70,7 +57,7 @@ df$edu[df$F5 == 4] <- 18
 df$edu[df$F5 == 5] <- NA
 table(df$edu, useNA='always')
 
-#世帯年収
+##世帯年収####
 table(df$F6, useNA='always')
 df$income <- NA
 df$income[df$F6 == 1] <- 0
@@ -88,7 +75,17 @@ df$income[df$F6 == 12] <- 2000
 df$income[df$F6 == 13] <- NA
 table(df$income, useNA='always')
 
-####地域SC関連項目###################
+##居住年数####
+table(df$SQ4, useNA='always')
+df$liv_year <- NA
+df$liv_year[df$SQ4 == 1] <- 1
+df$liv_year[df$SQ4 == 2] <- (1 + 5) / 2
+df$liv_year[df$SQ4 == 3] <- (5 + 10) / 2
+df$liv_year[df$SQ4 == 4] <- (10 + 20) / 2
+df$liv_year[df$SQ4 == 5] <- 20
+table(df$liv_year, useNA='always')
+
+#地域SC関連項目###################
 #地域への信頼（逆転）
 table(df$Q4_1, useNA='always')
 df$local_trust <- NA
@@ -100,7 +97,7 @@ df$local_trust[df$Q4_1 == 1] <- 5
 table(df$local_trust, useNA='always')
 
 
-####アウトカム#######################
+#アウトカム#######################
 #主観的幸福感
 #GULPの先行研究（埴淵編 2022:89）で、地方の方が大都市より幸福感が
 #低いという結果あり。
